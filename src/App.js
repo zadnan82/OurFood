5790d752983c277585d86492b59b942ff9584ba8
zadnan82 <zadnaniths@gmail.com>
2023-05-16 10:51:27 +0200

    Added Template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';  
+import Footer from './Components/Footer';
 import Home from './Components/Home';
 
 	function App() {
 	  return (
 	    <div className="App">
 	     <Home/>
+		 <Footer/>
 	    </div>
 	  );
 	}
