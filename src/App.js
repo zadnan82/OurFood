5cec162d8c6d48668bd9e2be80a1f20d4b8037e4
Lawe Zangena <lawza02@gmail.com>
2023-05-16 19:20:24 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';  
 import Footer from './Components/Footer';
 import Home from './Components/Home';
+import Contact from './Components/Contact';
 
 	function App() {
 	  return (
 	    <div className="App">
 	     <Home/>
 		 <Footer/>
+		 <Contact></Contact>
 	    </div>
 	  );
 	}
---
+++ src/Components/Contact.js
@@ -0,0 +1,142 @@
+import React, { Component } from 'react';
+
+export default class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <section id="contact" className="contact">
+          <div className="container" data-aos="fade-up">
+
+            <div className="section-header">
+              <h2>Contact</h2>
+              <p>Need Help? <span>Contact Us</span></p>
+            </div>
+
+            <div className="mb-3">
+              <iframe title="Google Maps" style={{ border: 0, width: '100%', height: '350px' }} src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" allowFullScreen></iframe>
+            </div>
+
+            <div className="row gy-4">
+
+              <div className="col-md-6">
+                <div className="info-item  d-flex align-items-center">
+                  <i className="icon bi bi-map flex-shrink-0"></i>
+                  <div>
+                    <h3>Our Address</h3>
+                    <p>A108 Adam Street, New York, NY 535022</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-6">
+                <div className="info-item d-flex align-items-center">
+                  <i className="icon bi bi-envelope flex-shrink-0"></i>
+                  <div>
+                    <h3>Email Us</h3>
+                    <p>contact@example.com</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-6">
+                <div className="info-item  d-flex align-items-center">
+                  <i className="icon bi bi-telephone flex-shrink-0"></i>
+                  <div>
+                    <h3>Call Us</h3>
+                    <p>+1 5589 55488 55</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-6">
+                <div className="info-item  d-flex align-items-center">
+                  <i className="icon bi bi-share flex-shrink-0"></i>
+                  <div>
+                    <h3>Opening Hours</h3>
+                    <div>
+                      <strong>Mon-Sat:</strong> 11AM - 23PM;
+                      <strong>Sunday:</strong> Closed
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+            <form action="forms/contact.php" method="post" role="form" className="php-email-form p-3 p-md-4">
+              <div className="row">
+                <div className="col-xl-6 form-group">
+                  <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
+                </div>
+                <div className="col-xl-6 form-group">
+                  <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
+                  </div>
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" required />
+          </div>
+          <div className="form-group">
+            <textarea className="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+          </div>
+          <div className="my-3">
+            <div className="loading">Loading</div>
+            <div className="error-message"></div>
+            <div className="sent-message">Your message has been sent. Thank you!</div>
+          </div>
+          <div className="text-center"><button type="submit">Send Message</button></div>
+        </form>
+
+      </div>
+    </section>
+
+    <footer id="footer" className="footer">
+      <div className="container">
+        <div className="row gy-3">
+          <div className="col-lg-3 col-md-6 d-flex">
+            <i className="bi bi-geo-alt icon"></i>
+            <div>
+              <h4>Address</h4>
+              <p>
+                A108 Adam Street <br />
+                New York, NY 535022 - US<br />
+              </p>
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-6 footer-links d-flex">
+            <i className="bi bi-telephone icon"></i>
+            <div>
+              <h4>Reservations</h4>
+              <p>
+                <strong>Phone:</strong> +1 5589 55488 55<br />
+                <strong>Email:</strong> info@example.com<br />
+              </p>
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-6 footer-links d-flex">
+            <i className="bi bi-clock icon"></i>
+            <div>
+              <h4>Opening Hours</h4>
+              <p>
+                <strong>Mon-Sat: 11AM</strong> - 23PM<br />
+                Sunday: Closed
+              </p>
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-6 footer-links">
+            <h4>Follow Us</h4>
+            <div className="social-links d-flex">
+              <a href="#" className="twitter"><i className="bi bi-twitter"></i></a>
+              <a href="#" className="facebook"><i className="bi bi-facebook"></i></a>
+              <a href="#" className="instagram"><i className="bi bi-instagram"></i></a>
+              <a href="#" className="linkedin"><i className="bi bi-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+);
+    }}
